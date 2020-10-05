f1e8aca1688f0accbf75e6759c777947e328b2d5
Lance Dall <ldall2009@gmail.com>
2020-10-05 13:07:17 -0700

    Implemented 3D Camera First Person Functionality

--- CS4450 Final Project/src/Main.java
+++ CS4450 Final Project/src/Main.java
@@ -1,27 +1,22 @@
-
-import static org.lwjgl.opengl.GL11.*;
 import org.lwjgl.input.Keyboard;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
+import static org.lwjgl.opengl.GL11.*;
+import org.lwjgl.util.glu.GLU;
 
 public class Main {
     
-    private static final int WINDOW_WIDTH = 640;
-    private static final int WINDOW_HEIGHT = 480;
+    private static final int SCREEN_WIDTH = 640;
+    private static final int SCREEN_HEIGHT = 480;
     
-    private static final int ESCAPE = Keyboard.KEY_ESCAPE;
-    private static final int FORWARD = Keyboard.KEY_W;
-    private static final int BACKWARD = Keyboard.KEY_S;
-    private static final int LEFT = Keyboard.KEY_A;
-    private static final int RIGHT = Keyboard.KEY_D;
-    private static final int UP = Keyboard.KEY_SPACE;
-    private static final int DOWN = Keyboard.KEY_LSHIFT;
+    private FPCameraController fp = new FPCameraController(0f, 0f, 0f);
+    private DisplayMode displayMode;
 
     public void start() {
         try {
             createWindow();
             initGL();
-            render();
+            fp.gameLoop();// render();
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -29,8 +24,15 @@ public class Main {
 
     private void createWindow() throws Exception {
         Display.setFullscreen(false);
-        Display.setDisplayMode(new DisplayMode(WINDOW_WIDTH, WINDOW_HEIGHT));
-        Display.setTitle("Final Project");
+        DisplayMode d[] = Display.getAvailableDisplayModes();
+        for (int i = 0; i < d.length; i++) {
+            if (d[i].getWidth() == SCREEN_WIDTH && d[i].getHeight() == SCREEN_HEIGHT && d[i].getBitsPerPixel() == 32) {
+                displayMode = d[i];
+                break;
+            }
+        }
+        Display.setDisplayMode(displayMode);
+        Display.setTitle("The Tony Trio - Final Project");
         Display.create();
     }
 
@@ -38,37 +40,13 @@ public class Main {
         glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
         glMatrixMode(GL_PROJECTION);
         glLoadIdentity();
-        glOrtho(0, WINDOW_WIDTH, 0, WINDOW_HEIGHT, 1, -1);
+        GLU.gluPerspective(100.0f, (float) displayMode.getWidth() / (float) displayMode.getHeight(), 0.1f, 300.0f);
         glMatrixMode(GL_MODELVIEW);
         glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
-    }
-
-    private void render() {
-
-        while (!Display.isCloseRequested()) {
-
-            if (Keyboard.isKeyDown(ESCAPE)) {
-                System.out.println("Exiting application...");
-                System.exit(0);
-            }
-
-            try {
-                glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-                glLoadIdentity();
-                glPointSize(10);
-
-                Display.update();
-                Display.sync(60);
-            } catch (Exception e) {
-            }
-        }
-        Display.destroy();
     }
 
     public static void main(String[] args) {
         Main program = new Main();
         program.start();
     }
-
-
 }
---
+++ CS4450 Final Project/src/Vector3Float.java
@@ -0,0 +1,11 @@
+//a class to hold our camera’s position in 3D space
+
+public class Vector3Float {
+    public float x, y, z;
+
+    public Vector3Float(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+}
