9f3320d062a158865ee4b435d3fcc96a27d6eab8
Lance Dall <ldall2009@gmail.com>
2020-10-05 21:02:50 -0700

    Rendered a Cube

--- CS4450 Final Project/src/FPCameraController.java
+++ CS4450 Final Project/src/FPCameraController.java
@@ -194,11 +194,58 @@ public class FPCameraController {
         try {
             glBegin(GL_QUADS);
             glColor3f(1.0f, 0.0f, 1.0f);
+            
             glVertex3f(1.0f, -1.0f, -1.0f);
             glVertex3f(-1.0f, -1.0f, -1.0f);
             glVertex3f(-1.0f, 1.0f, -1.0f);
             glVertex3f(1.0f, 1.0f, -1.0f);
             glEnd();
+            
+            glBegin(GL_QUADS);
+            glColor3f(0.7f, 0.2f, 1.0f);
+            
+            glVertex3f(1.0f, -1.0f, -3.0f);
+            glVertex3f(-1.0f, -1.0f, -3.0f);
+            glVertex3f(-1.0f, 1.0f, -3.0f);
+            glVertex3f(1.0f, 1.0f, -3.0f);
+            glEnd();
+            
+            glBegin(GL_QUADS);
+            glColor3f(0.0f, 0.0f, 1.0f);
+            
+            glVertex3f(1.0f, -1.0f, -1.0f);
+            glVertex3f(1.0f, -1.0f, -3.0f);
+            glVertex3f(1.0f, 1.0f, -3.0f);
+            glVertex3f(1.0f, 1.0f, -1.0f);
+            glEnd();
+            
+            glBegin(GL_QUADS);
+            glColor3f(1.0f, 0.0f, 0.0f);
+            
+            glVertex3f(-1.0f, -1.0f, -3.0f);
+            glVertex3f(-1.0f, -1.0f, -1.0f);
+            glVertex3f(-1.0f, 1.0f, -1.0f);
+            glVertex3f(-1.0f, 1.0f, -3.0f);
+            glEnd();
+            
+            glBegin(GL_QUADS);
+            glColor3f(1.0f, 0.5f, 0.5f);
+            
+            glVertex3f(1.0f, -1.0f, -1.0f);
+            glVertex3f(-1.0f, -1.0f, -1.0f);
+            glVertex3f(-1.0f, -1.0f, -3.0f);
+            glVertex3f(1.0f, -1.0f, -3.0f);
+            glEnd();
+            
+            glBegin(GL_QUADS);
+            glColor3f(0.0f, 1.0f, 0.0f);
+            
+            glVertex3f(1.0f, 1.0f, -1.0f);
+            glVertex3f(-1.0f, 1.0f, -1.0f);
+            glVertex3f(-1.0f, 1.0f, -3.0f);
+            glVertex3f(1.0f, 1.0f, -3.0f);
+            glEnd();
+            
         } catch (Exception e) { }
     }
 }
