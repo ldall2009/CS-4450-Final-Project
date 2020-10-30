2a7614d52f94a89d316d9e6823d9d84694133e7f
nandanvinjamury <nvinjamury@gmail.com>
2020-10-30 01:23:23 -0700

    chunks class and blocks class finished

--- CS4450 Final Project/src/Chunk.java
+++ CS4450 Final Project/src/Chunk.java
@@ -32,10 +32,100 @@ public class Chunk {
             for(float z = 0; z < CHUNK_SIZE; z++){
                 for(float y = 0; y < CHUNK_SIZE; y++){
                     VertexPositionData.put(createCube((float)(startX + x * CUBE_LENGTH), (float)(y*CUBE_LENGTH+(int)(CHUNK_SIZE*0.8)), (float)(startZ + z * CUBE_LENGTH)));
+                    VertexColorData.put(createCubeVertexCol(getCubeColor(blocks[(int)x][(int)y][(int)z])));
                 }
             }
         }
+        VertexColorData.flip();
+        VertexPositionData.flip();
+        glBindBuffer(GL_ARRAY_BUFFER, VBOVertexHandle);
+        glBufferData(GL_ARRAY_BUFFER, VertexPositionData, GL_STATIC_DRAW);
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+        glBindBuffer(GL_ARRAY_BUFFER, VBOColorHandle);
+        glBufferData(GL_ARRAY_BUFFER, VertexColorData, GL_STATIC_DRAW);
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
     }
     
+    private float[] createCubeVertexCol(float[] CubeColorArray){
+        float[] cubeColors = new float[CubeColorArray.length*4*6];
+        for(int i=0; i<cubeColors.length; i++){
+            cubeColors[i] = CubeColorArray[i % CubeColorArray.length];
+        }
+        return cubeColors;
+    }
+    
+    public static float[] createCube(float x, float y, float z){
+        int offset = CUBE_LENGTH/2;
+        return new float[] {
+            //top quad
+            x+offset, y+offset, z,
+            x-offset, y+offset, z,
+            x-offset, y+offset, z - CUBE_LENGTH,
+            x+offset, y+offset, z - CUBE_LENGTH,
+            //bottom quad
+            x+offset, y-offset, z - CUBE_LENGTH,
+            x-offset, y-offset, z - CUBE_LENGTH,
+            x-offset, y-offset, z,
+            x+offset, y-offset, z,
+            //front quad
+            x+offset, y+offset, z - CUBE_LENGTH,
+            x-offset, y+offset, z - CUBE_LENGTH,
+            x-offset, y-offset, z - CUBE_LENGTH,
+            x+offset, y-offset, z - CUBE_LENGTH,
+            //back quad
+            x+offset, y-offset, z,
+            x-offset, y-offset, z,
+            x-offset, y+offset, z,
+            x+offset, y+offset, z,
+            //left quad
+            x-offset, y+offset, z - CUBE_LENGTH,
+            x-offset, y+offset, z,
+            x-offset, y-offset, z,
+            x-offset, y-offset, z - CUBE_LENGTH,
+            //right quad
+            x+offset, y+offset, z,
+            x+offset, y+offset, z - CUBE_LENGTH,
+            x+offset, y-offset, z - CUBE_LENGTH,
+            x+offset, y-offset, z
+        };
+    }
+    
+    private float[] getCubeColor(Block block){
+        switch(block.getID()){
+            case 1:
+                return new float[]{0,1,0};
+            case 2:
+                return new float[]{1,0.5f,0};
+            case 3:
+                return new float[]{0,0,1};
+        }
+        return new float[]{1,1,1};
+    }
+    
+    public Chunk(int startX, int startY, int startZ){
+        r = new Random();
+        blocks = new Block[CHUNK_SIZE][CHUNK_SIZE][CHUNK_SIZE];
+        for(int x = 0; x < CHUNK_SIZE; x++){
+            for(int y = 0; y < CHUNK_SIZE; y++){
+                for(int z = 0; z < CHUNK_SIZE; z++){
+                    if(r.nextFloat()>0.7f){
+                        blocks[x][y][z] = new Block(Block.BlockType.Grass);
+                    } else if(r.nextFloat()>0.7f){
+                        blocks[x][y][z] = new Block(Block.BlockType.Dirt);
+                    } else if(r.nextFloat()>0.7f){
+                        blocks[x][y][z] = new Block(Block.BlockType.Water);
+                    } else {
+                        blocks[x][y][z] = new Block(Block.BlockType.Stone);
+                    }
+                }
+            }
+        }
+        VBOColorHandle = glGenBuffers();
+        VBOVertexHandle = glGenBuffers();
+        this.startX = startX;
+        this.startY = startY;
+        this.startZ = startZ;
+        rebuildMesh(startX,startY,startZ);
+    } 
     
 }
--- CS4450 Final Project/src/Main.java
+++ CS4450 Final Project/src/Main.java
@@ -74,6 +74,9 @@ public class Main {
         GLU.gluPerspective(100.0f, (float) displayMode.getWidth() / (float) displayMode.getHeight(), 0.1f, 300.0f);
         glMatrixMode(GL_MODELVIEW);
         glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
+        glEnableClientState(GL_VERTEX_ARRAY);
+        glEnableClientState(GL_COLOR_ARRAY);
+        glEnable(GL_DEPTH_TEST);
     }
 
     /***************************************************************
